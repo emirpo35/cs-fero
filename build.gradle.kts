517bd8ae4efaaedc472ec8acfdee57300c017b8b
emirpo35 <emir@logsis.net>
2026-08-18 16:15:16 +0300

    l

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/feroxx/Kekik-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/emirpo35/cs-fero")
 
         authors = listOf("keyiflerolsun")
     }
